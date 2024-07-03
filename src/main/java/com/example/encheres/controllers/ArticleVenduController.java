8b2fe01f72d37f01b3dc29dbbc23495c062057e7
Simon KERVADEC <simon.kervadec2024@campus-eni.fr>
2024-07-03 09:13:18 +0200

    commit Simon

--- src/main/java/com/example/encheres/controllers/ArticleVenduController.java
+++ src/main/java/com/example/encheres/controllers/ArticleVenduController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ArticleVenuController {
+public class ArticleVenduController {
 
 	@GetMapping("/vendre-article")
 	public String vendreArticle() {
