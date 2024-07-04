bbb42de5f23bc5ff40029ffd22cc6401951ca1c2
Heejung KIM <heejung.kim2024@campus-eni.fr>
2024-07-04 16:40:43 +0200

    xx

--- src/main/java/com/example/encheres/controllers/HomeController.java
+++ src/main/java/com/example/encheres/controllers/HomeController.java
@@ -9,26 +9,27 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.encheres.bll.ArticleVenduService;
 import com.example.encheres.bll.CategorieService;
+import com.example.encheres.bll.EnchereService;
+import com.example.encheres.bll.UtilisateurService;
 import com.example.encheres.bo.ArticleVendu;
 import com.example.encheres.bo.Categorie;
+import com.example.encheres.bo.Enchere;
 import com.example.encheres.bo.Utilisateur;
 
 @Controller
 @SessionAttributes({"utilisateurSession"})
 public class HomeController {
 	private ArticleVenduService articleVenduService;
-	private CategorieService categorieService;
-	private List <ArticleVendu> articles;
-	
-	public HomeController(ArticleVenduService articleVenduService, CategorieService categorieService,
-			List<ArticleVendu> articles) {
+	private UtilisateurService utilisateurService;
+
+
+	public HomeController(ArticleVenduService articleVenduService) {
 		this.articleVenduService = articleVenduService;
-		this.categorieService = categorieService;
-		this.articles = articles;
 	}
 
 	@Autowired
@@ -45,13 +46,28 @@ public class HomeController {
     }
     
     @GetMapping({"/","home","encheres","listes-articles"})
-    public String home(Model model) {
-    	List <ArticleVendu> articles = new ArrayList<ArticleVendu>();
-    	articles.add(new ArticleVendu(1, "Une pipe","ceci n est pas une pipe",LocalDate.now(),LocalDate.now(), 10, 110, new Categorie(1, "informatique"),new Utilisateur(),new ArrayList<enchere>()));
-    	articles.add(new ArticleVendu(2, "Table de salon" ,"une tres belle table en chene", "2024-07-01","2024-07-30", 100, 150, 1, 2, 1));
-    	model.addAttribute("articles",articles);
-		model.addAttribute("categories", categorieService.findAll());
-        return "home";
+    public String home(
+    	Model model) {
+    	List<ArticleVendu> articles = this.articleVenduService.findAll();
+    	System.out.println(articles);
+    	
+    	
+    	
+    	model.addAttribute("articles", articles);
+    	
+    	
+    	
+    	
+    	//ArticleVendu [noArticle=1, nomArticle=Table de salon, description=une tres belle table en chene, 
+    	//dateDebutEnchere=2024-07-01, dateFinEnchere=2024-07-30, prixInitial=100.0, prixVente=150.0, 
+    	//categorie=Categorie [noCategorie=1, libelle=null], acheteur=Utilisateur [noUtilisateur=2, pseudo=null, nom=null, prenom=null, email=null, telephone=null, rue=null, codePostal=null, ville=null, motDePasse=null, credit=0, administrateur=false], 
+    	//vendeur=Utilisateur [noUtilisateur=1, pseudo=null, nom=null, prenom=null, email=null, telephone=null, rue=null, codePostal=null, ville=null, motDePasse=null, credit=0, administrateur=false], encheres=[]], 
+    	
+    	return "home";
+    	
+    	
+    	
+    	
     }
 }
 
