742b9bea0c8c66bc99511a213fe12f8b9c988153
Ygreco <yanisg2002@gmail.com>
2024-07-04 15:06:12 +0200

    commit-XX : coorection retrait service regression + gestion retrait articles

--- src/main/java/com/example/encheres/bll/RetraitService.java
+++ src/main/java/com/example/encheres/bll/RetraitService.java
@@ -3,8 +3,6 @@ package com.example.encheres.bll;
 import com.example.encheres.bo.Retrait;
 
 public interface RetraitService {
-	void creerRetrait (Retrait retrait);
-	Retrait lectureRetrait (int noArticle);
-	
-
+	void create(Retrait retrait);
+	Retrait read(int noArticle);
 }
