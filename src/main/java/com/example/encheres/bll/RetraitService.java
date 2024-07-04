a39ed3a0078e60d7a8303398505bc4108f2bed36
Ygreco <yanisg2002@gmail.com>
2024-07-04 10:39:11 +0200

    fix pb

--- src/main/java/com/example/encheres/bll/RetraitService.java
+++ src/main/java/com/example/encheres/bll/RetraitService.java
@@ -1,11 +1,6 @@
 package com.example.encheres.bll;
 
-import com.example.encheres.bo.Enchere;
 import com.example.encheres.bo.Retrait;
-import com.example.encheres.dal.RetraitDao;
-import com.example.encheres.exception.BusinessException;
-
-import java.util.List;
 
 public interface RetraitService {
 	void create(Retrait retrait);
