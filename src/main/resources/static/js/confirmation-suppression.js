4d678fd6b78c82dad3880be6292148a116b94703
Lilian BOUMENDIL <lilian.boumendil2024@campus-eni.fr>
2024-07-05 16:46:13 +0200

    confirm

---
+++ src/main/resources/static/js/confirmation-suppression.js
@@ -0,0 +1,8 @@
+/**
+ * affichage d' une pop-up de confirmation de la suppression
+ * 
+ */
+
+function confirmation() {
+            return confirm("Êtes-vous sûr de vouloir visiter ce lien ?");
+            }
